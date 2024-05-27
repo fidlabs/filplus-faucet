bfc7a61ae04bef780b3e846907c9c3ff64732807
Grzegorz Strączek <grzegorz.straczek@neti-soft.com>
2024-05-27 11:11:41 +0200

    Remove unnecessary routing. Fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { ModalProvider } from "@/lib/providers/modal.provider";
 import Providers from "./providers";
 
