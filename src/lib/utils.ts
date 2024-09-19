eb3aaeb9e34f848f5da2530113657f8cf0978215
lukas <lukasz.wal@neti-soft.com>
2024-09-19 11:00:35 +0200

    Signature test

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,8 @@ import { zeroAddress } from "viem";
 const MESSAGE_TEXT =
   "Connect your Fil+ application with your wallet and give access to your Gitcoin passport";
 
-const DOMAIN_NAME = "Fil+ AutoAllocator";
-export const PRIMARY_TYPE = "FilCoinAddress";
+const DOMAIN_NAME = "Fil+ KYC";
+export const PRIMARY_TYPE = "KycApproval";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -17,14 +17,16 @@ export function createMessage(clientId: string) {
   const issuedAt = new Date();
   const daysToExpire = env.NEXT_PUBLIC_DAYS_TO_EXPIRE;
   const expiresAt = new Date(
-    issuedAt.getTime() + daysToExpire * 24 * 60 * 60 * 1000,
+    issuedAt.getTime() + daysToExpire * 24 * 60 * 60 * 1000
   );
 
   return {
     message: MESSAGE_TEXT,
-    client_fil_address: clientId,
+    client_id: clientId,
     issued_at: issuedAt.toISOString(),
     expires_at: expiresAt.toISOString(),
+    allocator_repo_name: "",
+    allocator_repo_owner: "",
   };
 }
 
@@ -39,9 +41,11 @@ export function createDomain(chainId?: number, version?: string) {
 
 export function createFilecoinTypes() {
   return {
-    FilCoinAddress: [
+    KycApproval: [
       { name: "message", type: "string" },
-      { name: "client_fil_address", type: "string" },
+      { name: "client_id", type: "string" },
+      { name: "allocator_repo_name", type: "string" },
+      { name: "allocator_repo_owner", type: "string" },
       { name: "issued_at", type: "string" },
       { name: "expires_at", type: "string" },
     ],
