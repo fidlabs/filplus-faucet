864a454c20d9b514ff0a0d3832ad279519fa0874
lukas <lukasz.wal@neti-soft.com>
2024-09-19 11:39:15 +0200

    Test

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ const MESSAGE_TEXT =
   "Connect your Fil+ application with your wallet and give access to your Gitcoin passport";
 
 const DOMAIN_NAME = "Fil+ KYC";
-export const PRIMARY_TYPE = "KycApproval";
+export const PRIMARY_TYPE = "KycAutoallocationApproval";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -22,11 +22,9 @@ export function createMessage(clientId: string) {
 
   return {
     message: MESSAGE_TEXT,
-    client_id: clientId,
+    client_fil_address: clientId,
     issued_at: issuedAt.toISOString(),
     expires_at: expiresAt.toISOString(),
-    allocator_repo_name: "",
-    allocator_repo_owner: "",
   };
 }
 
@@ -41,11 +39,9 @@ export function createDomain(chainId?: number, version?: string) {
 
 export function createFilecoinTypes() {
   return {
-    KycApproval: [
+    KycAutoallocationApproval: [
       { name: "message", type: "string" },
-      { name: "client_id", type: "string" },
-      { name: "allocator_repo_name", type: "string" },
-      { name: "allocator_repo_owner", type: "string" },
+      { name: "client_fil_address", type: "string" },
       { name: "issued_at", type: "string" },
       { name: "expires_at", type: "string" },
     ],
