1ed6f95bae003e7c0ead168ba486d81f69548833
lukas <lukasz.wal@neti-soft.com>
2024-09-20 11:02:55 +0200

    typo

--- src/components/ui/filecoinAddressAccept.tsx
+++ src/components/ui/filecoinAddressAccept.tsx
@@ -28,7 +28,7 @@ const FileCoinAddressAccept: FC<{
   return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center w-4/5">
       <p className="block text-gray-700 font-bold mb-2 text-lg text-center">
-        {`Please enter you're Filcoin Address:`}
+        {`Please enter your Filcoin Address:`}
       </p>
       <Input className="mb-2" type="text" value={value} onChange={onChange} />
       <Button className="mx-auto" onClick={onClick} disabled={disabled}>
