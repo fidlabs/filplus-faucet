1117164125d9d640362f33ed5ded18829091433c
Łukasz Wal <143998006+lukasz-wal@users.noreply.github.com>
2025-03-27 14:10:34 +0100

    Implement Google Analytics (#15)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ModalProvider } from "@/lib/providers/modal.provider";
 import Providers from "./providers";
 import { Suspense } from "react";
 import { LoadingProvider } from "@/lib/providers/loading.provider.client";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { env } from "@/env";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,12 +24,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content={env.NEXT_PUBLIC_GSV_ID}
+        />
       </head>
       <body className={inter.className}>
         <Suspense>
           <ModalProvider>
             <LoadingProvider>
               <div className="flex flex-col">
+                <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />
                 <Providers>{children}</Providers>
               </div>
             </LoadingProvider>
